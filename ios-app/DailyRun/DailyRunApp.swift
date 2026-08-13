c81b154179ef691d25703a87d32a56f6e6a6ee53
Daniel Chang <dawichan@ucsc.edu>
2026-08-13 16:01:42 -0700

    Add SwiftUI app skeleton under ios-app
    Xcode 26 App template, targeting iOS 18.0. Uses file-system-synchronized
    groups, so on-disk layout is the project structure.
    
    - Rename the wizard's ContentView to TodayView
    - Move Assets.xcassets under Resources/
    - Set display name to "Daily Run"
    - Ignore xcuserdata and build output

---
+++ ios-app/DailyRun/DailyRunApp.swift
@@ -0,0 +1,17 @@
+//
+//  DailyRunApp.swift
+//  DailyRun
+//
+//  Created by Daniel Chang on 8/13/26.
+//
+
+import SwiftUI
+
+@main
+struct DailyRunApp: App {
+    var body: some Scene {
+        WindowGroup {
+            TodayView()
+        }
+    }
+}
